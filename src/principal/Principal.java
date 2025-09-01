ae5a6aaf8e9ae1742b9e0b27c105a131fc355e54
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-09-01 19:48:57 -0300

    feat: Adiciona as classes

---
+++ src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import multiplathread_1.MultiplaThread;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        System.out.println("Antes da Janela:" + Thread.activeCount());
+        MultiplaThread janela = new MultiplaThread();
+        janela.setVisible(true);
+        System.out.println("Depois da Janela:" + Thread.activeCount());
+    }
+}
